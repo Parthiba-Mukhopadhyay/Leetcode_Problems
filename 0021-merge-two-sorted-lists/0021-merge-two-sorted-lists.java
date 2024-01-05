835200e1b3a8d89ed60d2348ef035cef35ae7f21
Parthiba Mukhopadhyay <meparthiba@gmail.com>
2024-01-05 23:01:51 +0530

    Update 0021-merge-two-sorted-lists.java

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,7 +10,8 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-       if(list1==null)
+       //check the list
+        if(list1==null)
            return list2;
        else if(list2==null)
            return list1;
